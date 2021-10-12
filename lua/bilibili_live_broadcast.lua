b3b6313fdb3d80593cf78b35e083580206b8911e
PangPangPangPangPang <446964321@qq.com>
2021-10-13 00:12:42 +0800

    ignore error

--- lua/bilibili_live_broadcast.lua
+++ lua/bilibili_live_broadcast.lua
@@ -8,18 +8,18 @@ M.config = {
 }
 
 M.setup = function(args)
-    local config = M.config
+	local config = M.config
 	for key, value in pairs(args) do
 		config[key] = value
 	end
 end
 
 M.run = function(args)
-    local config = M.config
-    if config.job ~= nil then
+	local config = M.config
+	if config.job ~= nil then
 		print("Already start!")
-        return
-    end
+		return
+	end
 	if vim.fn.executable("node") ~= 1 then
 		print("Need install node!")
 		return
@@ -32,22 +32,27 @@ M.run = function(args)
 
 	local cmd = string.format("node %s/build/index.js %s", vim.g.bilibili_root, args)
 	local function print_stdout(_, data, _)
-		local msg = vim.fn.json_decode(data)
-		config.handler(msg)
+		local succ, msg = pcall(vim.fn.json_decode, data)
+		if succ then
+			config.handler(msg)
+		end
 	end
 	local function print_err(_, data, _)
-		local msg = vim.fn.json_decode(data)
-		require("notify")(actions[msg.msg], "error", { title = msg.type })
+		local succ, msg = pcall(vim.fn.json_decode, data)
+		if succ then
+			require("notify")(actions[msg.msg], "error", { title = msg.type })
+		end
 	end
+
 	local job_id = vim.fn.jobstart(cmd, { on_stdout = print_stdout, on_stderr = print_err })
-    config.job = job_id
+	config.job = job_id
 end
 M.stop = function()
-    local config = M.config
-    if config.job ~= nil then
-        vim.fn.jobstop(config.job)
+	local config = M.config
+	if config.job ~= nil then
+		vim.fn.jobstop(config.job)
 		print("Stopped!")
-        return
-    end
+		return
+	end
 end
 return M
