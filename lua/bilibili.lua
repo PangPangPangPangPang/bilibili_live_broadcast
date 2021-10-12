230cd0c90d59bf4fdb93cb4b68126fc274f95bb0
Max <wangyefeng@bytedance.com>
2021-10-12 14:55:37 +0800

    update

--- lua/bilibili.lua
+++ lua/bilibili.lua
@@ -1,25 +1,40 @@
 local M = {}
+local actions = require("bilibili.utils").actions
+
+M.config = {
+	handler = function(msg)
+		require("notify")(msg.msg, "info", { title = actions[msg.type] })
+	end,
+}
+
+M.setup = function(args)
+    local config = M.config
+	for key, value in pairs(args) do
+		config[key] = value
+	end
+end
 
 M.run = function(args)
-    if vim.fn.executable('node') ~= 1 then
-        print('Need install node!')
-        return;
-    end
-    local present, _ = pcall(require, 'notify')
-    if present ~= true then
-        print('Need install nofity!')
-        return
-    end
+    local handler = M.config.handler
+	if vim.fn.executable("node") ~= 1 then
+		print("Need install node!")
+		return
+	end
+	local present, _ = pcall(require, "notify")
+	if present ~= true then
+		print("Need install notify!")
+		return
+	end
 
-    local cmd = string.format("node %s/build/index.js %s", vim.g.bilibili_root, args)
-    local function print_stdout(_, data, _)
-        local msg = vim.fn.json_decode(data)
-        require("notify")(msg.msg, "info", { title = msg.type })
-    end
-    local function print_err(_, data, _)
-        local msg = vim.fn.json_decode(data)
-        require("notify")(msg.msg, "error", { title = msg.type })
-    end
-    vim.fn.jobstart(cmd, { on_stdout = print_stdout, on_stderr = print_err })
+	local cmd = string.format("node %s/build/index.js %s", vim.g.bilibili_root, args)
+	local function print_stdout(_, data, _)
+		local msg = vim.fn.json_decode(data)
+		handler(msg)
+	end
+	local function print_err(_, data, _)
+		local msg = vim.fn.json_decode(data)
+		require("notify")(actions[msg.msg], "error", { title = msg.type })
+	end
+	vim.fn.jobstart(cmd, { on_stdout = print_stdout, on_stderr = print_err })
 end
 return M
